646bbfc065a137bcfb58ab707bae0ada259bc045
RScupp <1ryanscupp@gmail.com>
2018-07-20 01:24:59 -0400

    fix name on editing

--- public/js/app.js
+++ public/js/app.js
@@ -105,12 +105,30 @@ angular.module("contactsApp", ['ngRoute'])
         }
 
         $scope.saveContact = function(contact) {
-            Contacts.editContact(contact);
-            $scope.editMode = false;
-            $scope.contactFormUrl = "";
+            if(checkform(contact))
+                {
+                    Contacts.editContact(contact);
+                    $scope.editMode = false;
+                    $scope.contactFormUrl = "";
+                }
         }
 
         $scope.deleteContact = function(contactId) {
             Contacts.deleteContact(contactId);
         }
     });
+function checkform(form) {
+    // get all the inputs within the submitted form
+    var inputs = document.getElementsByTagName("input");
+    for (var i = 0; i < inputs.length; i++) {
+        // only validate the inputs that have the required attribute
+        if(inputs[i].hasAttribute("required")){
+            if(inputs[i].value == ""){
+                // found an empty field that is required
+                alert("Please fill all required fields");
+                return false;
+            }
+        }
+    }
+    return true;
+}
